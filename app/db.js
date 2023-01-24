d0424371b83ef6bea1592471ce025899dd2e786e
Aleksey Dvornikov <lesha.dvornikov1@gmail.com>
2023-01-24 14:26:26 +0200

    crud generator

---
+++ app/db.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const pg = require('pg');
+
+const pool = new pg.Pool({
+    host: 'db',
+    port: 5432,
+    database: 'example',
+    user: 'user',
+    password: 'password',
+});
+
+module.exports = (table) => ({
+      read(id, fields = ['*']) {
+        const names = fields.join(', ');
+        const sql = `SELECT ${names} FROM ${table}`;
+        if (!id) return pool.query(sql);
+        return pool.query(`${sql} WHERE id = $1`, [id]);
+      },
+  
+      async create({ ...record }) {
+        const keys = Object.keys(record);
+        const nums = new Array(keys.length);
+        const data = new Array(keys.length);
+        let i = 0;
+        for (const key of keys) {
+            data[i] = record[key]; 
+            nums[i] = `$${++i}`;
+        }
+        const fields = '"' + keys.join('", "') + '"';
+        const sql = `INSERT INTO ${table} (${fields}) VALUES (${nums.join(", ")})`;
+        return pool.query(sql, data);
+      },
+  
+      async update(id, { ...record }) {
+        const keys = Object.keys(record);
+        const updates = new Array(keys.length);
+        const data = new Array(keys.length);
+        let i = 0;
+        for (const key of keys) {
+            data[i] = record[key]; 
+            updates[i] = `${key} = $${++i}`;
+        }
+        const sql = `UPDATE ${table} SET ${updates.join(', ')} WHERE id = $${++i}`;
+        data.push(id);
+        return pool.query(sql, data);
+      },
+  
+      delete(id) {
+        const sql = `DELETE FROM ${table} WHERE id = $1`;
+        return pool.query(sql, [id]);
+      },
+});
--- app/main.js
+++ app/main.js
@@ -1,62 +1,33 @@
 'use strict';
 
 const http = require('node:http');
-const pg = require('pg');
-const hash = require('./hash.js');
 const receiveArgs = require('./body.js');
+const db = require('./db.js');
 
 const PORT = 3000;
 
-const pool = new pg.Pool({
-  host: 'db',
-  port: 5432,
-  database: 'example',
-  user: 'user',
-  password: 'password',
-});
-
 const routing = {
-  user: {
-    get(id) {
-      if (!id) return pool.query('SELECT id, login FROM users');
-      const sql = 'SELECT id, login FROM users WHERE id = $1';
-      return pool.query(sql, [id]);
-    },
+  user: db('users')
+}
 
-    async post({ login, password }) {
-      const sql = 'INSERT INTO users (login, password) VALUES ($1, $2)';
-      const passwordHash = await hash(password);
-      return pool.query(sql, [login, passwordHash]);
-    },
-
-    async put(id, { login, password }) {
-      const sql = 'UPDATE users SET login = $1, password = $2 WHERE id = $3';
-      const passwordHash = await hash(password);
-      return pool.query(sql, [login, passwordHash, id]);
-    },
-
-    delete(id) {
-      const sql = 'DELETE FROM users WHERE id = $1';
-      return pool.query(sql, [id]);
-    },
-  },
-};
+const crud = { get: 'read', post: 'create', put: 'update', delete: 'delete' };
 
 http.createServer(async (req, res) => {
   const { method, url, socket } = req;
   const [name, id] = url.substring(1).split('/');
   const entity = routing[name];
   if(!entity) res.send('Not Found');
-  const handler = entity[method.toLowerCase()];
+  const procedure = crud[method.toLowerCase()];
+  const handler = entity[procedure];
   if(!handler) res.status(404).send('Not Found');
   const args = [];
   const src = handler.toString();
   const substr = src.substring(0, src.indexOf(')'));
   if(substr.includes('(id')) args.push(id);
   if(substr.includes('{')) args.push(await receiveArgs(req));
-  console.log(`${socket.remoteAddress} ${methid} ${url}`);
+  console.log(`${socket.remoteAddress} ${method} ${url}`);
   const result = await handler(...args);
-  res.end(JSON.stringify(result));
+  res.end(JSON.stringify(result.rows));
 }).listen(PORT);
 
 console.log(`Listen on port ${PORT}`);

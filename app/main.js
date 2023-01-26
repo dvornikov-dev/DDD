88eaf673af6d68626eac0dc11edec50ba10ba5c5
Aleksey Dvornikov <lesha.dvornikov1@gmail.com>
2023-01-26 13:18:53 +0200

    folder architecture

--- app/main.js
+++ app/main.js
@@ -1,14 +1,22 @@
 'use strict';
 
 const db = require('./db.js');
+const fsp = require('node:fs').promises;
+const path = require('node:path');
 const server = require('./transport/http.js');
 
 const PORT = 3000;
- 
-const routing = {
-  user: require('./services/users.js'),
-  city: db('city'),
-  county: db('country'),
-}
 
+const apiPath = path.join(__dirname, 'services');
+const routing = {};
+
+(async () => {
+  const files =  await fsp.readdir(apiPath);
+  for (const file of files) {
+    if (!file.endsWith('.js')) continue;
+    const filePath = path.join(apiPath, file);
+    const entity = path.basename(file, '.js');
+    routing[entity] = require(filePath);
+  }
+})();
 server(routing, PORT);
